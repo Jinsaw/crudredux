6ff2f071b3488e3a4e2a9b22e5a7212d5ff74217
Ronald Shumery <ronaldshumery@gmail.com>
2020-07-31 17:03:07 -0400

    agregando otros componentes y react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Fragment>
-      <Header />
-    </Fragment>
+    <Router>
+      <Header />  
+      <div className= "container mt-5">
+        <Switch>
+          <Route exact path= "/" component= {Productos}/>
+          <Route exact path= "/productos/nuevo" component= {NuevoProducto}/>
+          <Route exact path= "/productos/editar/:id" component= {EditarProducto}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+//REACT-ROUTER-DOM todo lo que este fuera del switch se va a utilizar en todas las paginas
 
 export default App;
