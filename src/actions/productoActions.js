9383ed9a403994152130589cbbb9c780dab55fd7
Ronald Shumery <ronaldshumery@gmail.com>
2020-08-04 16:44:03 -0400

    agregando productos al state y manejar un posible error

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -6,8 +6,28 @@ import {
 
 //crear nuevos productos
 export function crearNuevoProductoAction(producto) {
-    return () => {
-        const {nombre, precio} = producto
-        console.log(nombre, precio);
+    return (dispatch) => {
+        dispatch(agregarProducto());
+        try {
+            dispatch( agregarProductoExito(producto) );
+        } catch (error) {
+            dispatch( agregarProductoError(true) );
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO
+}) 
+
+//Si el producto se guarda en la db
+const agregarProductoExito = (producto) => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+})
+
+//Si hubo un error
+const agregarProductoError = (estado) => ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado
+})
