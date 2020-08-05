dd054963ba41cf62004e94a00b778a2df2f0d77d
Ronald Shumery <ronaldshumery@gmail.com>
2020-08-05 16:15:57 -0400

    creando el action para eliminar

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -4,7 +4,10 @@ import {
     AGREGAR_PRODUCTO_ERROR,
     COMENZAR_DESCARGA_PRODUCTOS,
     DESCARGA_PRODUCTOS_EXITO,
-    DESCARGA_PRODUCTOS_ERROR
+    DESCARGA_PRODUCTOS_ERROR,
+    OBTENER_PRODUCTO_ELIMINAR,
+    PRODUCTO_ELIMINADO_EXITO,
+    PRODUCTO_ELIMINADO_ERROR
 } from '../types';
 
 import clienteAxios from '../config/axios';
@@ -86,4 +89,17 @@ const descargaProductosExitosa = (productos) => ({
 const descargaProductosError = () => ({
     type: DESCARGA_PRODUCTOS_ERROR,
     payload: true
+})
+
+//Seleccionar y elimina un producto
+export function borrarProductoAction(id) {
+    return async (dispatch) => {
+        dispatch( obtenerProductoEliminar(id) );
+
+    }
+}
+
+const obtenerProductoEliminar = (id) => ({
+    type: OBTENER_PRODUCTO_ELIMINAR,
+    payload: id
 })
